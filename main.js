35186506c0c8ca3e262dba6a0596b597c3bc31d2
S1yngys <S1yngys@users.noreply.github.com>
2020-12-15 13:50:36 +0600

    init

---
+++ main.js
@@ -0,0 +1,47 @@
+// const sidebar = document.getElementById("sidebar");
+// const nav = document.querySelector('.nav2')
+// const arrow = document.querySelector('.exit')
+// console.log(sidebar)
+// function openMenu() {
+//     sidebar.style.display = "flex";
+//     nav.style.display = "flex"
+//     arrow.style.display = 'block';
+//     document.querySelector('.menu__elements').style.display = 'none'
+//     document.querySelector('.menu__nav').style.display = 'none'
+// }
+const headerCoffee = new Vivus(
+    'header__coffee',
+    {
+      type: 'sync',
+      duration: 200,
+
+    },
+
+  );
+//   const headerCoffee = new Vivus(
+//     'header__coffee',
+//     {
+//       type: 'sync',
+//       duration: 200,
+
+//     },
+
+//   );
+//   const headerCoffee = new Vivus(
+//     'beans',
+//     {
+//       type: 'sync',
+//       duration: 200,
+
+//     },
+
+//   );
+//   const headerCoffee = new Vivus(
+//     'header__coffee',
+//     {
+//       type: 'sync',
+//       duration: 200,
+
+//     },
+
+//   );
